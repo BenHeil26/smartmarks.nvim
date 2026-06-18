6f79d6123d953b1d3a4aeac1f0ec8e55c08f2eed
Benji Heil <ben.heil.dev@gmail.com>
2026-06-17 20:00:53 -0400

    fix keymap

--- lua/smartmarks.lua
+++ lua/smartmarks.lua
@@ -2,7 +2,7 @@ local M = {}
 
 function M.open_window()
   local w1 = vim.api.nvim_open_win(0, false,
-    { relative = 'win', row = 3, col = 3, width = 40, height = 4 })
+    { relative = 'cursor', row = 3, col = 3, width = 40, height = 4 })
 end
 
 function M.setup(opts)
@@ -12,7 +12,7 @@ function M.setup(opts)
 
   local keymap = opts.keymap or "<leader>ow"
 
-  vim.keymap.set("n", keymap, "OpenWindow")
+  vim.keymap.set("n", keymap, M.open_window)
 end
 
 return M
